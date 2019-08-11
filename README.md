f3ac78cff0c3dac655b59143243722ab7255d5ec
Robert <zeng444@163.com>
2019-08-11 10:35:23 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# phalcon_db_reconnection
-phalcon db reconnection
+## 场景
+
+- 常驻内存程序使用，解决长时间不操作造成的掉线、抛异常、造成的程序退出
+
+## 原理
+
+- 客户端连接在MYSQL设置的interactive_timeout之间内没有任何操作，会被主动断开，造成操作报错
+- 捕捉数据库出现的CR_SERVER_GONE_ERROR、CR_SERVER_LOST两类报错，并自动重连，并重新执行之前想执行的SQL操作
+
+## 使用
+
+> 析构函数增加了max_retry_connect参数，申明最大重连次数，此参数默认值2
+
+```php
+<?php
+use Janfish\Phalcon\Db\Adapter\Pdo\Mysql as Mysql;
+
+$di->setShared('db', function () {
+    return new Mysql([
+        'adapter' => 'Mysql',
+        'host' => 'localhost',
+        'port' => '3306',
+        'username' => 'root',
+        'password' => 'root',
+        'dbname' => 'test',
+        'max_retry_connect' => 2,
+    ]);
+});
+```
+
+## 参考资料
+
+https://dev.mysql.com/doc/refman/5.7/en/client-error-reference.html#error_cr_server_gone_error
+https://dev.mysql.com/doc/refman/5.7/en/client-error-reference.html#error_cr_server_lost
+
+
---
+++ src/Adapter/Pdo/Mysql.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Janfish\Phalcon\Db\Adapter\Pdo;
+
+use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
+
+/**
+ * Author:Robert
+ *
+ * Class Mysql
+ * @package Core\Db\Adapter\Pdo
+ */
+class Mysql extends PdoMysql
+{
+
+    /**
+     * Author:Robert
+     *
+     * @var int
+     */
+    private $_reconnectTime = 0;
+
+    /**
+     * MySQL server has gone away
+     * https://dev.mysql.com/doc/refman/5.7/en/client-error-reference.html#error_cr_server_gone_error
+     */
+    const  CR_SERVER_GONE_ERROR = 2006;
+
+    /**
+     *  Lost connection to MySQL server during query
+     * https://dev.mysql.com/doc/refman/5.7/en/client-error-reference.html#error_cr_server_lost
+     */
+    const  CR_SERVER_LOST = 2013;
+
+    /**
+     * MAX_RETRY_CONNECT_COUNT
+     */
+    protected $maxRetryConnectTime = 2;
+
+
+    /**
+     * Mysql constructor.
+     * @param $descriptor
+     */
+    public function __construct($descriptor)
+    {
+        if (is_array($descriptor)) {
+            if (isset($descriptor['max_retry_connect'])) {
+                $this->maxRetryConnectTime = $descriptor['max_retry_connect'];
+            }
+        }
+        parent::__construct($descriptor);
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param $error
+     * @return bool
+     */
+    public function isConnectionError($error): bool
+    {
+        return isset($error[1]) && in_array($error[1], [self::CR_SERVER_GONE_ERROR, self::CR_SERVER_LOST]);
+    }
+
+
+    /**
+     * Author:Robert
+     *
+     * @return bool
+     */
+    public function reconnect(): bool
+    {
+        if ($this->_reconnectTime > $this->maxRetryConnectTime) {
+            throw new \PDOException('Retry connection failed');
+        }
+        if (!$this->connect()) {
+            $this->_reconnectTime++;
+        }
+        $this->_reconnectTime = 0;
+        return true;
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param bool $nesting
+     * @return bool
+     */
+    public function begin($nesting = true): bool
+    {
+        try {
+            return parent::begin($nesting);
+        } catch (\PDOException $exception) {
+            if (!$this->isConnectionError($exception->errorInfo)) {
+                throw $exception;
+            }
+            $this->reconnect();
+            return $this->begin($nesting);
+        }
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param bool $nesting
+     * @return bool
+     */
+    public function rollback($nesting = true)
+    {
+        try {
+            return parent::rollback($nesting);
+        } catch (\PDOException $exception) {
+            if (!$this->isConnectionError($exception->errorInfo)) {
+                throw $exception;
+            }
+            $this->reconnect();
+            return $this->rollback($nesting);
+        }
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param bool $nesting
+     * @return bool
+     */
+    public function commit($nesting = true)
+    {
+        try {
+            return parent::commit($nesting);
+        } catch (\PDOException $exception) {
+            if (!$this->isConnectionError($exception->errorInfo)) {
+                throw $exception;
+            }
+            $this->reconnect();
+            return $this->commit($nesting);
+        }
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param string $sqlStatement
+     * @param null $bindParams
+     * @param null $bindTypes
+     * @return bool|\Phalcon\Db\ResultInterface
+     */
+    public function query($sqlStatement, $bindParams = null, $bindTypes = null)
+    {
+        try {
+            return parent::query($sqlStatement, $bindParams, $bindTypes);
+        } catch (\PDOException $exception) {
+            if (!$this->isConnectionError($exception->errorInfo)) {
+                throw $exception;
+            }
+            $this->connect();
+            return $this->query($sqlStatement, $bindParams, $bindTypes);
+        }
+    }
+
+    /**
+     * Author:Robert
+     *
+     * @param string $sqlStatement
+     * @param null $bindParams
+     * @param null $bindTypes
+     * @return bool
+     */
+    public function execute($sqlStatement, $bindParams = null, $bindTypes = null): bool
+    {
+        try {
+            return parent::execute($sqlStatement, $bindParams, $bindTypes);
+        } catch (\PDOException $exception) {
+            if (!$this->isConnectionError($exception->errorInfo)) {
+                throw $exception;
+            }
+            $this->connect();
+            return $this->execute($sqlStatement, $bindParams, $bindTypes);
+        }
+    }
+}
